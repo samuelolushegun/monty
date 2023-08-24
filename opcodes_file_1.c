b2dd1b37d0617694977af5bb534dbd79e8f2fa81
Kingsley Usa <kingsident@gmail.com>
2023-08-24 08:28:24 +0100

    Moved opcodes

--- opcodes_file_1.c
+++ opcodes_file_1.c
@@ -29,32 +29,6 @@ void add_opcode(const char *opcode, void (*f)(stack_t **stack, unsigned int line
 	}
 }
 
-
-void push(stack_t **stack, unsigned int line_number) {
-	// Implementation of push instruction
-	// Modify the stack as needed
-	// Use line_number as required
-	// NOTICE THAT this function will use variable arg
-}
-
-void pall(stack_t **stack, unsigned int line_number) {
-	// Implementation of pall instruction
-	// Modify the stack as needed
-	// Use line_number as required
-}
-
-void pint(stack_t **stack, unsigned int line_number) {
-	// Implementation of pint instruction
-	// Modify the stack as needed
-	// Use line_number as required
-}
-
-void pop(stack_t **stack, unsigned int line_number) {
-	// Implementation of pop instruction
-	// Modify the stack as needed
-	// Use line_number as required
-}
-
 void swap(stack_t **stack, unsigned int line_number) {
 	// Implementation of swap instruction
 	// Modify the stack as needed
